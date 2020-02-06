8b3c82a534a4833c4ed685eadc2978e0234ed63b
snek <40864710+snekplex@users.noreply.github.com>
2020-02-05 21:31:52 -0600

    Added an AppContainer component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './scss/App.scss';
 
 import AppHeader from './components/header';
+import AppContainer from './components/container';
 
 function App() {
   return (
     <div className="App">
       <AppHeader/>
+      <AppContainer/>
     </div>
   );
 }
---
+++ src/components/container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../scss/AppContainer.scss';
+
+class AppContainer extends React.Component {
+  render () {
+    return (
+      <div className="app-container">
+        af
+      </div>
+    )
+  }
+}
+
+export default AppContainer;
