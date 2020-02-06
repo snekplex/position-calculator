4e16ccde09b0d16160e2cac936308334dfd6a28b
snek <40864710+snekplex@users.noreply.github.com>
2020-02-05 21:18:25 -0600

    Added an app header to project

---
+++ src/components/header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../scss/AppHeader.scss';
+
+class AppHeader extends React.Component {
+  render() {
+    return (
+      <header className="app-header">
+        <div className="header-app-name">
+          <div id="app-header-logo">Position Calc</div>
+        </div>
+        <div className="header-left-links">
+          <ul className="header-link-list">
+            <li>
+              <a href="/">
+                <img 
+                  className="github-32-logo" 
+                  alt="Github Logo"
+                  src={window.location.origin + '/GitHub-Mark-Light-32px.png'}>
+                </img>
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    )
+  }
+}
+
+export default AppHeader;
