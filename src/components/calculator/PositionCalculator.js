bd214f9ebfb8f6710a7cef7a5554183b862f3a09
snek <40864710+snekplex@users.noreply.github.com>
2020-02-05 22:43:48 -0600

    Created form with calculator inputs

--- src/components/calculator/PositionCalculator.js
+++ src/components/calculator/PositionCalculator.js
@@ -6,6 +6,28 @@ class PositionCalculator extends React.Component {
   render () {
     return (
       <div className="position-calc">
+        <div className="position-calc-header">
+          Position Calculator
+        </div>
+        <div className="position-calc-body">
+          <form className="position-calc-form">
+            Price of stock <br/>
+            <input className="calc-txt-input" type="text" name="stockPrice"/>
+            Amount of stock <br/>
+            <input className="calc-txt-input" type="text" name="stockAmount"/>
+            Sell gain price <br/>
+            <input className="calc-txt-input" type="text" name="sellGainPrice"/>
+            Sell loss price <br/>
+            <input className="calc-txt-input" type="text" name="sellLossPrice"/>
+            Sell gain percent <br/>
+            <input className="calc-txt-input" type="text" name="sellGainPercent"/>
+            Sell loss percent <br/>
+            <input className="calc-txt-input" type="text" name="sellLossPercent"/>
+          </form>
+        </div>
+        <div className="position-calc-footer">
+          <button className="calc-submit-btn">Calculate</button>
+        </div>
       </div>
     )
   }
